883319f45e69ac7a263c5156012adcaf672af3e4
AlexTaietti <taiettialex@yahoo.it>
2020-08-27 20:14:22 +0100

    added .babelrc file + slight change in transpile process

--- gulpfile.js
+++ gulpfile.js
@@ -15,7 +15,7 @@ function style () {
 function script () {
 
 	return gulp.src('./src/es/*.js')
-		.pipe(babel({ plugins: ['@babel/plugin-proposal-class-properties', '@babel/plugin-transform-destructuring'], presets: ['@babel/env'] }))
+		.pipe(babel())
 		.pipe(gulp.dest('./dist/js'))
 		.pipe(BS.stream());
 
